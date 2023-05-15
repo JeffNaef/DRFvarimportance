c50f9bd2630cb02af710838e562da08883fffa1f
Jeff <jeffrey.naef@stat.math.ethz.ch>
2023-05-15 17:27:09 +0200

    initial comit

---
+++ drfnew_v2.R
@@ -0,0 +1,154 @@
+drfCI <- function(X, Y, B, sampling = "binomial",...) {
+  
+  n <- dim(X)[1]
+  
+  # compute point estimator and DRF per halfsample
+  # weightsb: B times n matrix of weights
+  DRFlist <- lapply(seq_len(B), function(b) {
+    
+    # half-sample index
+    indexb <- if (sampling == "binomial") {
+      seq_len(n)[as.logical(rbinom(n, size = 1, prob = 0.5))]
+    } else {
+      sample(seq_len(n), floor(n / 2), replace = FALSE)
+    }
+    
+    ## Using normal Bootstrap on the data and refitting DRF
+    DRFb <- 
+      drf(X = X[indexb, , drop = F], Y = Y[indexb, , drop = F],
+          ci.group.size = 1, ...)
+    
+    
+    return(list(DRF = DRFb, indices = indexb))
+  })
+  
+  return(list(DRFlist = DRFlist, X = X, Y = Y) )
+}
+
+
+predictdrf<- function(DRF, x, functional = NULL, ...) {
+  
+  ntest <- nrow(x)
+  n <- nrow(DRF$Y)
+  
+  
+  weightsb <- lapply(DRF$DRFlist, function(l) {
+    
+    weightsbfinal <- Matrix(0, nrow = ntest, ncol = n , sparse = TRUE)
+    
+    weightsbfinal[, l$indices] <- predict(l$DRF, x)$weights 
+    
+    return(weightsbfinal)
+  })
+  
+  
+  weightsall <- Reduce("+", weightsb) / length(weightsb)
+  
+  if (!is.null(functional)) {
+    ## Achtung: This so far works only for one x!  
+    stopifnot("Not yet implemented for several x" = ntest == 1)
+    
+    thetahatb <- 
+      lapply(weightsb, function(w)  
+        functional(weights = w , X = DRF$X, Y = DRF$Y, x = x))
+    thetahatbforvar <- do.call(rbind, thetahatb)
+    thetahat <- functional(weights = weightsall , X = DRF$X, Y = DRF$Y, x = x)
+    thetahat <- matrix(thetahat, nrow = dim(x)[1])
+    var_est <- if (dim(thetahat)[2] > 1){  
+      a <- sweep(thetahatbforvar, 2, thetahat, FUN = "-")
+      crossprod(a, a) / B
+    } else {
+      mean((c(thetahatbforvar) - c(thetahat)) ^ 2) 
+    }
+    
+    return(list(weights = weightsall, thetahat = thetahat, weightsb = weightsb, 
+                var_est = var_est ))
+    
+  } else {
+    return(list(weights = weightsall, weightsb = weightsb ))
+  }
+}
+
+
+
+condindtest <- function(X,Y,Z, x, B,...){
+  ## Tests whether Y ind of Z | X=x
+  
+  listofargs<-list(...)
+  
+  if ("response.scaling" %in% names(listofargs)){
+    response.scaling <- unname(unlist(listofargs["response.scaling"]))
+  }else{
+    # Default in DRF
+    response.scaling<-T
+  }
+  
+  
+  # Fit 3 DRFs, one for (Y,Z) jointly given X, with CIs, one
+  # for Y|X only and one for Z|X only 
+  DRF<-drfCI(X=X,Y=cbind(Y,Z), B=B,...)
+  DRF1 <- drf(X=X,Y=Y, ...)
+  DRF2 <- drf(X=X,Y=Z, ...)
+  
+  ## predict to obtain mu(x) and mu_1(x)*mu_2(x)
+  DRFpred<-predictdrf(DRF, x=x)
+  w<-DRFpred$weights # mu(x)
+  w1<-predict(DRF1,newdata=x)$weights #mu_1(x)
+  w2<-predict(DRF2,newdata=x)$weights #mu_2(x)
+  
+  ## Contine here!!!
+  if (response.scaling==T){
+    Y.transformed <- scale(Y)
+    Z.transformed <- scale(Z)
+  }else{
+    Y.transformed<-Y
+    Z.transformed<-Z
+  }
+  
+  bandwidth.Y <- drf:::medianHeuristic(Y.transformed)
+  bandwidth.Z <- drf:::medianHeuristic(Z.transformed)
+  
+  # bandwidth.Y <- 1
+  # bandwidth.Z <- 1
+  
+  k_Y<-rbfdot(sigma=bandwidth.Y)
+  k_Z<-rbfdot(sigma=bandwidth.Z)
+  K=kernelMatrix(k_Y, Y, y = Y)
+  L=kernelMatrix(k_Z, Z, y = Z)
+  
+  
+  ## Continue here: Idea ||m_{n,1} x \mu_{n,2} - mu_n||^2 should have the same asymptotic distribution as 
+  ## ||mu_n - mu||^2 which in turn should have the same (asymptotic) distribution as ||mu_n^{S} - mu_n||^2
+  
+  
+  # ||m_{n,1} x \mu_{n,2} - mu_n||^2 
+  teststat<-diag(w%*%(K*L)%*%t(w)) + diag(w1%*%K%*%t(w1))*diag(w2%*%L%*%t(w2)) - 2* diag(w%*%((K%*%t(w1))*(L%*%t(w2))))
+  # Using same DRF
+  #teststat<-diag(w%*%(K*L)%*%t(w)) + diag(w%*%K%*%t(w))*diag(w%*%L%*%t(w)) - 2* diag(w%*%((K%*%t(w))*(L%*%t(w))))
+  
+  
+  
+  teststat<-matrix(teststat, ncol=nrow(x), byrow = T)
+  
+  nulldist<-lapply(DRFpred$weightsb, function(wb){
+    
+    # w: ntest x n matrix of weights
+    #diag(t(w)%*%K%*%L%*%w) + diag(t(w1)%*%K%*%w1)*diag(t(w2)%*%K%*%w2) - 2* t(w)%*%((K%*%w1)*(K%*%w2))
+    # ||mu_n^{S} - mu_n||^2 = || (wb-w)'%*% (k_1, ...., k_n) ||^2
+    #diag(t(wb)%*%K%*%L%*%wb) + diag(t(w)%*%K%*%L%*%w) - 2* t(w)%*%((K%*%w1)*(K%*%w2))
+    # ||mu_n^{S} - mu_n||^2
+    diag( (wb-w)%*%(K*L)%*%t(wb-w) )
+    
+    
+  })
+  
+  nulldist <- do.call(rbind,nulldist)
+  
+  pval <- mean(sapply(seq_len(ncol(nulldist)), function(j) teststat[,j] <= nulldist[,j] )) 
+  
+  #pval<-mean(teststat < nulldist)
+  
+  
+  return(list(pval = pval, nulldist = nulldist, teststat = teststat))
+  
+}
